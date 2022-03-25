5da12e48173984ec7133d7ddd6debd693e1c4814
Giuliani Martinez Herrera <54904733+SebasGMH@users.noreply.github.com>
2022-03-25 11:27:48 -0400

    Add files via upload

---
+++ YellowTaxiData.py
@@ -0,0 +1,26 @@
+"""
+Name:  Giuliani Martinez Herrera
+Email: giuliani.martinezherrer04@myhunter.cuny.edu
+Resources:  Used python.org as a reminder of Python 3 print statements.
+Program 8 Yellow taxi data
+"""
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+
+def import_data(file_name) -> pd.DataFrame:
+    return
+
+def add_tip_time_features(df) -> pd.DataFrame:
+    return
+
+def impute_numeric_cols(df, x_num_cols) -> pd.DataFrame:
+    return
+
+def transform_numeric_cols(df_num, degree_num=2) -> pd.DataFrame:
+    return
+
+def fit_linear_regression(x_train, y_train):
+    return
+
+def predict_using_trained_model(mod, x, y):
+    return
