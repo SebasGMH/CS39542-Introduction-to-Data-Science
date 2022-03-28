870d1fad752b1be19021233503909f1fc3b26269
Giuliani Martinez Herrera <54904733+SebasGMH@users.noreply.github.com>
2022-03-28 15:52:18 -0400

    Add files via upload

--- YellowTaxiData.py
+++ YellowTaxiData.py
@@ -6,12 +6,27 @@ Program 8 Yellow taxi data
 """
 import pandas as pd
 from sklearn.linear_model import LinearRegression
+from datetime import date
 
 def import_data(file_name) -> pd.DataFrame:
-    return
+    df = pd.read_csv(file_name)
+    df = df[df['total_amount']>0]
+    return df
 
+# df = import_data('Yellow_taxi.csv')
+# print(df)
 def add_tip_time_features(df) -> pd.DataFrame:
-    return
+    #3 new col
+    percent_tip_data= 100*df['tip_amount']/(df['total_amount']-df['tip_amount'])
+    df['percent_tip']= percent_tip_data
+    #convert col to datetime
+    dropoff= pd.to_datetime(df['tpep_dropoff_datetime'])
+    pickup = pd.to_datetime(df['tpep_pickup_datetime'])
+    duration_data = dropoff-pickup
+    df['duration']= duration_data
+    dayofweek_data= pickup.weekday()
+    df['dayofweek'] = dayofweek_data
+    return df
 
 def impute_numeric_cols(df, x_num_cols) -> pd.DataFrame:
     return
