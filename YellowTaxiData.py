646a2d7e900aff2ccfac344e602dedae62c2e3ab
Giuliani Martinez Herrera <54904733+SebasGMH@users.noreply.github.com>
2022-03-29 02:39:36 -0400

    Add files via upload

--- YellowTaxiData.py
+++ YellowTaxiData.py
@@ -4,8 +4,11 @@ Email: giuliani.martinezherrer04@myhunter.cuny.edu
 Resources:  Used python.org as a reminder of Python 3 print statements.
 Program 8 Yellow taxi data
 """
+from numpy import NaN
 import pandas as pd
 from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.metrics import r2_score,mean_squared_error
 from datetime import date
 
 def import_data(file_name) -> pd.DataFrame:
@@ -17,6 +20,7 @@ def import_data(file_name) -> pd.DataFrame:
 # print(df)
 def add_tip_time_features(df) -> pd.DataFrame:
     #3 new col
+    #broken lmao
     percent_tip_data= 100*df['tip_amount']/(df['total_amount']-df['tip_amount'])
     df['percent_tip']= percent_tip_data
     #convert col to datetime
@@ -24,18 +28,27 @@ def add_tip_time_features(df) -> pd.DataFrame:
     pickup = pd.to_datetime(df['tpep_pickup_datetime'])
     duration_data = dropoff-pickup
     df['duration']= duration_data
-    dayofweek_data= pickup.weekday()
-    df['dayofweek'] = dayofweek_data
+    #dayofweek_data= pickup.date.weekday()
+    df['dayofweek'] = duration_data
     return df
 
 def impute_numeric_cols(df, x_num_cols) -> pd.DataFrame:
-    return
+    #Missing data in the columns x_num_cols are replaced with the 
+    # median of the column. Returns a DataFrame containing only the imputed numerical columns from input df
+    for col in x_num_cols:
+        df[col]=df[col].replace(NaN,df[col].median())
+
+    return df[x_num_cols]
 
 def transform_numeric_cols(df_num, degree_num=2) -> pd.DataFrame:
-    return
+    transformer = PolynomialFeatures(degree=degree_num,include_bias=False)
+    X = transformer.fit_transform(df_num)
+    return X
 
 def fit_linear_regression(x_train, y_train):
-    return
+    reg = LinearRegression().fit(x_train, y_train)
+    return reg.intercept_,reg.coef_,reg
 
 def predict_using_trained_model(mod, x, y):
-    return
+    x_modded = mod.predict(x)
+    return mean_squared_error(y,x_modded),r2_score(y,x_modded)
