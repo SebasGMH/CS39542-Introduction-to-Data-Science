7357992760a324bff1124e22119e8324cab4f777
Giuliani Martinez Herrera <54904733+SebasGMH@users.noreply.github.com>
2022-04-06 23:00:22 -0400

    Add files via upload

---
+++ ClassifyingDigits.py
@@ -0,0 +1,23 @@
+"""
+Name:  Giuliani Martinez Herrera
+Email: giuliani.martinezherrer04@myhunter.cuny.edu
+Resources:  Used python.org as a reminder of Python 3 print statements.
+Classifying Digits
+"""
+def select_data(data, target, labels = [0,1]):
+    return
+
+def split_data(data, target, test_size = 0.25, random_state = 21):
+    return
+
+def fit_model(x_train, y_train, model_type='logreg'):
+    return
+
+def predict_model(mod_pkl, xes):
+    return
+
+def score_model(mod_pkl,xes,yes):
+    return
+
+def compare_models(data, target, test_size = 0.25, random_state = 21, models = ['logreg','nbayes','svm','rforest']):
+    return
